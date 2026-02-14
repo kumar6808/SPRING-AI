963b8aa39ee16a9fbf89bbab548740a5dbc5b6a5
kumar <kr895280@gmail.com>
2026-02-14 13:21:31 +0530

    Image to text

---
+++ src/main/java/dev/kumar/Spring_ai/multimodal/ImageDetection.java
@@ -0,0 +1,38 @@
+package dev.kumar.Spring_ai.multimodal;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImageDetection {
+
+
+    private  final ChatClient  chatClient ;
+    @Value("classpath:images/alex-safareli-tlvehmbP0ZM-unsplash.jpg")
+    Resource sampleImage;
+
+
+    public ImageDetection(ChatClient.Builder  builder) {
+
+        this.chatClient = builder.build();
+    }
+
+
+    @GetMapping("image-to-text")
+    public String imageToText() {
+        return chatClient.prompt().user(u -> {
+                            u.text("Can you please describe what you see in the image?");
+                            u.media(MimeTypeUtils.IMAGE_JPEG, sampleImage);
+                        }
+                ).call()
+                .content();
+
+    }
+}
+
+
